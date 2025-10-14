d865ae4452346697336b5107d5132eeb58298c06
msotodev <mariosotomor.dev@gmail.com>
2025-10-14 00:14:05 -0600

    Unused parameter

--- src/components/sections/FormDocument.tsx
+++ src/components/sections/FormDocument.tsx
@@ -1,10 +1,10 @@
 import type { DocumentItemProps } from "../../types/document";
 
-export default function FormDocument({id, title} : DocumentItemProps) {
+export default function FormDocument({ title }: DocumentItemProps) {
     return (
         <article>
             <input type="text" value={title} />
-            
+
             <form id="form-new-document" className="form-new-document">
                 <input type="hidden" name="documentId" value="1" />
             </form>
